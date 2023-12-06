1465724509c8c376be069b26a90d76412a44fdc5
ytia001 <yiken36@gmail.com>
2023-12-06 23:37:15 +0800

    Test2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import * as icons from "./images/icons";
 
 
+
+
+
 function Piece({onTilePiece}){
   // let source = "./images/icons/" + onTilePiece +".png";
   // console.log(source);
@@ -15,17 +18,16 @@ function Piece({onTilePiece}){
 }
 
 
-function Tile({isWhite,onBoardPiece,onhandleClick}){
-
+function Tile({isWhite,onBoardPiece,onhandleClick,onActive}){
   if(isWhite){
     return(
-      <div className="tile-style-white" onClick={onhandleClick}>
+      <div className={`tile-style-white ${onActive? 'active' : ''}`} onClick={onhandleClick}>
         <Piece onTilePiece = {onBoardPiece} />
       </div>
     );
   }else{
     return(
-      <div className="tile-style-wood" onClick={onhandleClick}>
+      <div className={`tile-style-wood ${onActive? 'active' : ''}`} onClick={onhandleClick}>
         <Piece onTilePiece = {onBoardPiece} />
       </div>
     );
@@ -33,8 +35,7 @@ function Tile({isWhite,onBoardPiece,onhandleClick}){
 }
 
 function Board(){
-  const[start,setStart] = useState(null);
-  const[end, setEnd] = useState(null);
+  const[start,setStart] = useState(Array(2).fill(null));
   const[boardState, setBoardState] = useState([
     ["b_rook","b_knight","b_bishop","b_queen","b_king","b_bishop","b_knight","b_rook"],
     ["b_pawn","b_pawn","b_pawn","b_pawn","b_pawn","b_pawn","b_pawn","b_pawn"],
@@ -45,11 +46,34 @@ function Board(){
     ["w_pawn","w_pawn","w_pawn","w_pawn","w_pawn","w_pawn","w_pawn","w_pawn"],
     ["w_rook","w_knight","w_bishop","w_queen","w_king","w_bishop","w_knight","w_rook"]
   ]);
+  const [onGoing,setOnGoing] = useState(false);
+
 
   function handleClick(position){
-    console.log(position.r);
-    console.log(position.c);
+    if((onGoing && start[0] === position[0] && start[1] === position[1]) || (!onGoing && !boardState[position[0]][position[1]]) ){
+      if(onGoing) setOnGoing(!onGoing);
+      return;
+    }
+
+    if(!onGoing){
+      setStart(position);
+    }
+    else{
+      // build a helper function that determine if the move is allowed or not , if yes proceed , if not return
+      if(boardState[position[0]][position[1]] != null && boardState[position[0]][position[1]][0] === boardState[start[0]][start[1]][0]){
+        setOnGoing(!onGoing);
+        return;
+      }
+
+
+      let newBoardState = boardState.slice();
+      newBoardState[position[0]][position[1]] = boardState[start[0]][start[1]];
+      newBoardState[start[0]][start[1]] = null;
+      setBoardState(newBoardState);
+    }
+    setOnGoing(!onGoing);
   }
+
 
   let boardTemplate = [];
   let isWhite = true;
@@ -57,7 +81,7 @@ function Board(){
   for(let r = 0 ; r < 8 ; r++){
     let rowTemplate = [];
     for(let c = 0; c < 8 ; c++){
-      rowTemplate.push(<Tile isWhite = {isWhite} onBoardPiece = {boardState[r][c]} onhandleClick = {() => handleClick({r,c})} />);
+      rowTemplate.push(<Tile isWhite = {isWhite} onBoardPiece = {boardState[r][c]} onhandleClick = {() => handleClick([r,c])} onActive = { onGoing && start[0] ===r && start[1] === c? true : false}/>);
       isWhite = !isWhite;
     }
     boardTemplate.push(rowTemplate);
